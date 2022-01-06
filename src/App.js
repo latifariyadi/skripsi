ab7bbb8626a56b6225c70f8c68fdeb2ba6e1220e
jemblonganvalley <jemblonganvalley@gmail.com>
2022-01-06 14:00:18 +0700

    fadli _ update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import DashboardSiswa from './componenents/dashboard_siswa/dashboard_siswa'
-import './App.css';
+import React from "react";
+import DashboardSiswa from "./componenents/dashboard_siswa/dashboard_siswa";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
       <DashboardSiswa />
     </div>
   );
